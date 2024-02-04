88ae9a8329279261ba1564401499c301f5986d70
Ivan Zhang <ivanzhang1999@gmail.com>
2024-02-03 22:53:41 -0800

    Vday changes

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-const x = 0;
-const y = 0;
-const button = document.getElementById('button');
+document.addEventListener("DOMContentLoaded", function() {
+    const button_no = document.getElementById('button_no');
+    const button_yes = document.getElementById('button_yes');
 
-// Get the position of the button
-const buttonRect = button.getBoundingClientRect();
+    //Adding functionality to swap position of buttons if they click no
+    button_no.addEventListener('click', function() {
+        // Get the position of the button
+        const button_no_rect = button_no.getBoundingClientRect();
+        const button_yes_rect = button_yes.getBoundingClientRect();
+        
+        const initial_yes_top = button_yes_rect.top + 'px';
+        const initial_yes_left = button_yes_rect.left + 'px';
 
-// Log the x and y coordinates
-console.log('Button X:', buttonRect.x);
-console.log('Button Y:', buttonRect.y);
+        button_yes.style.position = 'absolute';
+        button_yes.style.top = button_no_rect.top + 'px';
+        button_yes.style.left = button_no_rect.left + 'px';
 
-button_yes.addEventListener('click', function() {
-    window.open('https://colonist.io/#2bx2');
+        button_no.style.position = 'absolute';
+        button_no.style.top = initial_yes_top;
+        button_no.style.left = initial_yes_left;
+        
+
+    });
 });
 
-function move_button(top, left) {
-    button.style.position = 'absolute'
-    button.style.top = top;
-    button.style.left = left;
-}
-
-document.addEventListener('mousemove', function(event){
-    const x = event.clientX;
-    const y = event.clientY;
-
-});
-
-button.addEventListener('mouseover', function(event){
-    const new_button_y = getRandomInt(0, window.innerHeight);
-    const new_button_x = getRandomInt(0, window.innerWidth);
-    move_button(new_button_y,new_button_x);
-    console.log('x and y' + new_button_x + ' ' + new_button_y);
-});
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
